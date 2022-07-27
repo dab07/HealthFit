4acff02d08cfb2d297b0ed1352c999cae73a8f11
dab07 <harshitsh0711@gmail.com>
2022-07-27 12:10:15 +0530

    [wip] exchanging auth for token

--- app/src/main/java/com/example/androidfit/MainActivity.java
+++ app/src/main/java/com/example/androidfit/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.androidfit;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,46 +15,83 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.androidfit.databinding.ActivityMainBinding;
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.FitnessOptions;
-import com.google.android.gms.fitness.RecordingClient;
-import com.google.android.gms.fitness.data.Bucket;
-import com.google.android.gms.fitness.data.DataPoint;
-import com.google.android.gms.fitness.data.DataSet;
-import com.google.android.gms.fitness.data.DataSource;
 import com.google.android.gms.fitness.data.DataType;
-import com.google.android.gms.fitness.data.Field;
-import com.google.android.gms.fitness.request.DataReadRequest;
-import com.google.android.gms.fitness.result.DataReadResponse;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.client.HttpClient;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.impl.client.DefaultHttpClient;
 
-import static com.google.android.gms.fitness.data.HealthFields.FIELD_BLOOD_PRESSURE_DIASTOLIC;
 import net.danlew.android.joda.JodaTimeAndroid;
 
-import org.joda.time.DateTime;
-
-import java.util.List;
-import java.util.Locale;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Objects;
-import java.util.concurrent.TimeUnit;
 
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity<authCode> extends AppCompatActivity {
     private static final int REQUEST_OAUTH_REQUEST_CODE = 0x1001;
-    private static final String TAG = "MainActivity";
+    private static final String TAG = "Fit";
     private TextView counter;
     private TextView weekCounter;
     private AppBarConfiguration appBarConfiguration;
+    private GoogleApiClient mGoogleApiClient;
+    private static final int RC_GET_AUTH_CODE = 9003;
+    private String mAccessToken;
+    private long mTokenExpired;
 
-    static DataSource ESTIMATED_STEP_DELTAS = new DataSource.Builder()
-            .setDataType(DataType.TYPE_STEP_COUNT_DELTA)
-            .setType(DataSource.TYPE_DERIVED)
-            .setStreamName("estimated_steps")
-            .setAppPackageName("com.google.android.gms")
-            .build();
+    private static final String DATA_URL = "https://www.googleapis.com/fitness/v1/users/me/dataSources";
+    private static final String USER_AUTHORIZATION_URL = "https://accounts.google.com/o/oauth2/v2/auth";
+    private static final String ACCESS_TOKEN_URL = "https://oauth2.googleapis.com/token";
+
+    private static final String CLIENT_ID = "323623763070-sqarr6qf4ju9361she7etj71f9ool81a.apps.googleusercontent.com";
+    private static final String CLIENT_SECRET = "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";
+
+    public GoogleFitDataTypes[] getFitDataTypes() {
+        return new GoogleFitDataTypes[] {
+                GoogleFitDataTypes.BODY_HEIGHT,
+                GoogleFitDataTypes.BODY_WEIGHT,
+                GoogleFitDataTypes.CALORIES_BURNED,
+                GoogleFitDataTypes.GEOPOSITION,
+                GoogleFitDataTypes.HEART_RATE,
+                GoogleFitDataTypes.PHYSICAL_ACTIVITY,
+                GoogleFitDataTypes.SPEED,
+                GoogleFitDataTypes.STEP_COUNT
+        };
+    }
+    public enum GoogleFitDataTypes implements FitDataType {
+
+        BODY_HEIGHT("derived:com.google.height:com.google.android.gms:merge_height"),
+        BODY_WEIGHT("derived:com.google.weight:com.google.android.gms:merge_weight"),
+        CALORIES_BURNED("derived:com.google.calories.expended:com.google.android.gms:merge_calories_expended"),
+        GEOPOSITION("derived:com.google.location.sample:com.google.android.gms:merge_location_samples"),
+        HEART_RATE("derived:com.google.heart_rate.bpm:com.google.android.gms:merge_heart_rate_bpm"),
+        PHYSICAL_ACTIVITY("derived:com.google.activity.segment:com.google.android.gms:merge_activity_segments"),
+        SPEED("derived:com.google.speed:com.google.android.gms:merge_speed"),
+        STEP_COUNT("derived:com.google.step_count.delta:com.google.android.gms:merge_step_deltas");
+
+        private final String streamId;
+
+        GoogleFitDataTypes(String streamId) {
+            this.streamId = streamId;
+        }
+
+        public String getStreamId() {
+            return streamId;
+        }
+    }
+
+    public MainActivity() throws IOException {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,16 +110,22 @@ public class MainActivity extends AppCompatActivity {
                 .setAction("Action", null).show());
         JodaTimeAndroid.init(this);
 
-        counter = findViewById(R.id.counter);
-        weekCounter = findViewById(R.id.week_counter);
-
         if (hasFitPermission()) {
-            readStepCountDelta();
-            readHistoricStepCount();
+//            readStepBackEnd();
         } else {
             requestFitnessPermission();
         }
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestScopes(new Scope(Scopes.DRIVE_APPFOLDER))
+                .requestServerAuthCode(CLIENT_ID)
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this , (GoogleApiClient.OnConnectionFailedListener) this )
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
     }
+
     private boolean hasFitPermission() {
         FitnessOptions fitnessOptions = getFitnessSignInOptions();
         return GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions);
@@ -108,116 +149,60 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == REQUEST_OAUTH_REQUEST_CODE) {
-                Log.i(TAG, "Fitness permission granted");
-                subscribeStepCount();
-                readStepCountDelta(); // Read today's data
-                readHistoricStepCount(); // Read last weeks data
+//        if (resultCode == Activity.RESULT_OK) {
+//            if (requestCode == REQUEST_OAUTH_REQUEST_CODE) {
+//                Log.i(TAG, "Fitness permission granted");
+//                subscribeStepCount();
+//                readStepBackEnd();
+////                readStepCountDelta(); // Read today's data
+////                readHistoricStepCount(); // Read last weeks data
+//            }
+//        } else {
+//            Log.i(TAG, "Fitness permission denied");
+//        }
+        if (requestCode == RC_GET_AUTH_CODE) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                String authCode = account.getServerAuthCode();
+                String idToken = account.getIdToken();
+                readStepBackEnd(authCode, idToken);
+
+            } catch (ApiException e) {
+                Log.w(TAG, "Sign-in failed", e);
             }
-        } else {
-            Log.i(TAG, "Fitness permission denied");
         }
     }
+
+    private void readStepBackEnd(String authCode, String idToken) {
+        HttpClient httpClient = new DefaultHttpClient();
+        String CLIENT_SECRET_FILE = "/Users/hs/AndroidStudioProjects/client_secret_323623763070-sqarr6qf4ju9361she7etj71f9ool81a.apps.googleusercontent.com.json";
+        GoogleClientSecrets clientSecrets =
+                GoogleClientSecrets.load(
+                        JacksonFactory.getDefaultInstance(), new FileReader(CLIENT_SECRET_FILE));
+        GoogleTokenResponse tokenResponse =
+                new GoogleAuthorizationCodeTokenRequest(
+                        new NetHttpTransport(),
+                        JacksonFactory.getDefaultInstance(),
+                        "https://oauth2.googleapis.com/token",
+                        CLIENT_ID,
+                        CLIENT_SECRET,
+                        authCode,
+                        "REDIRECT_URI")  // Specify the same redirect URI that you use with your web
+                        // app. If you don't have a web version of your app, you can
+                        // specify an empty string.
+                        .execute();
+
+        String accessToken = tokenResponse.getAccessToken();
+
+
+    }
+
     private void subscribeStepCount() {
         Fitness.getRecordingClient(this, Objects.requireNonNull(GoogleSignIn.getLastSignedInAccount(this)))
                 .subscribe(DataType.TYPE_STEP_COUNT_CUMULATIVE);
     }
-    private void readStepCountDelta() {
-        if (!hasFitPermission()) {
-            requestFitnessPermission();
-            return;
-        }
 
-        Fitness.getHistoryClient(this, Objects.requireNonNull(GoogleSignIn.getLastSignedInAccount(this)))
-                .readDailyTotal(DataType.AGGREGATE_STEP_COUNT_DELTA)
-                .addOnSuccessListener(
-                        dataSet -> {
-                            long total =
-                                    dataSet.isEmpty()
-                                            ? 0
-                                            : dataSet.getDataPoints().get(0).getValue(Field.FIELD_STEPS).asInt();
-                            Log.d(TAG, "Total steps: " + total);
-                            //display counts on screen
-                            counter.setText(String.format(Locale.ENGLISH, "%d", total));
-                        })
-                .addOnFailureListener(
-                        e -> Log.w(TAG, "There was a problem getting the step count.", e));
-    }
-    private void readHistoricStepCount() {
-        if (!hasFitPermission()) {
-            requestFitnessPermission();
-            return;
-        }
-
-        Fitness.getHistoryClient(this, Objects.requireNonNull(GoogleSignIn.getLastSignedInAccount(this)))
-                .readData(queryFitnessData())
-                .addOnSuccessListener(
-                        this::printData)
-                .addOnFailureListener(
-                        e -> Log.e(TAG, "There was a problem reading the historic data.", e));
-    }
-
-    public static DataReadRequest queryFitnessData() {
-        DateTime dt = new DateTime().withTimeAtStartOfDay();
-        long endTime = dt.getMillis();
-        long startTime = dt.minusWeeks(1).getMillis();
-
-        return new DataReadRequest.Builder()
-                .aggregate(ESTIMATED_STEP_DELTAS, DataType.AGGREGATE_STEP_COUNT_DELTA)
-                .bucketByTime(1, TimeUnit.DAYS)
-                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
-                .build();
-    }
-
-    public void printData(DataReadResponse dataReadResult) {
-        StringBuilder result = new StringBuilder();
-        if (dataReadResult.getBuckets().size() > 0) {
-            Log.i(TAG, "Number of returned buckets of DataSets is: " + dataReadResult.getBuckets().size());
-            for (Bucket bucket : dataReadResult.getBuckets()) {
-                List<DataSet> dataSets = bucket.getDataSets();
-                for (DataSet dataSet : dataSets) {
-                    result.append(formatDataSet(dataSet));
-                }
-            }
-        } else if (dataReadResult.getDataSets().size() > 0) {
-            Log.i(TAG, "Number of returned DataSets is: " + dataReadResult.getDataSets().size());
-            for (DataSet dataSet : dataReadResult.getDataSets()) {
-                result.append(formatDataSet(dataSet));
-            }
-        }
-        Log.d(TAG, "result: " + result);
-        weekCounter.setText(result);
-    }
-    private static String formatDataSet(DataSet dataSet) {
-        StringBuilder result = new StringBuilder();
-
-        for (DataPoint dp : dataSet.getDataPoints()) {
-            org.joda.time.DateTime sDT = new org.joda.time.DateTime(dp.getStartTime(TimeUnit.MILLISECONDS));
-            org.joda.time.DateTime eDT = new org.joda.time.DateTime(dp.getEndTime(TimeUnit.MILLISECONDS));
-
-            result.append(
-                    String.format(
-                            Locale.ENGLISH,
-                            "%s %s to %s %s\n",
-                            sDT.dayOfWeek().getAsShortText(),
-                            sDT.toLocalTime().toString("HH:mm"),
-                            eDT.dayOfWeek().getAsShortText(),
-                            eDT.toLocalTime().toString("HH:mm")
-                    )
-            );
-
-            result.append(
-                    String.format(
-                            Locale.ENGLISH,
-                            "%s: %s %s\n",
-                            sDT.dayOfWeek().getAsShortText(),
-                            dp.getValue(dp.getDataType().getFields().get(0)),
-                            dp.getDataType().getFields().get(0).getName()));
-        }
-
-        return String.valueOf(result);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(@NonNull Menu menu) {
@@ -232,14 +217,6 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-        if (id == R.id.action_read_data) {
-            readStepCountDelta();
-            return true;
-        } else if (id == R.id.action_read_historic_data) {
-            readHistoricStepCount();
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
     @Override
